7021f4deee6ecea11b6654b51d5bd0205d8235d4
takieddine12 <algeriahero22@gmail.com>
2024-03-07 18:06:11 +0100

    new changes

--- app/src/main/java/com/bottom/files/MainActivity.java
+++ app/src/main/java/com/bottom/files/MainActivity.java
@@ -1,73 +1,95 @@
 package com.bottom.files;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private File fileName;
-    private Button saveFile,loadFile;
+
+    private File tempoFile;
+    private File finalFile;
     private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        saveFile  = findViewById(R.id.saveFile);
-        loadFile  = findViewById(R.id.loadFile);
+        Button saveFile = findViewById(R.id.saveFile);
+        Button loadFile = findViewById(R.id.loadFile);
         videoView = findViewById(R.id.vidView);
 
-        saveFile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveVideoToCache();
-            }
-        });
-        loadFile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadSavedVideo();
-            }
-        });
+        saveFile.setOnClickListener(v -> {
+            try {
+                File dcimTestDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "TEST");
+                if (!dcimTestDir.exists()) {
+                    boolean wasSuccessful =  dcimTestDir.mkdirs();
+                }
+
+                File destFile = new File(dcimTestDir, "Sample.mp4");
+                if (!destFile.exists()) {
+                    boolean wasSuccessful = destFile.createNewFile();
+                }
+
+                FileInputStream inputStream = new FileInputStream(tempoFile);
+                FileOutputStream outputStream = new FileOutputStream(destFile);
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                Log.d("TAG", "File moved successfully to: " + destFile.getAbsolutePath());
+                finalFile  = destFile;
+                // Close streams
+                inputStream.close();
+                outputStream.close();
 
 
+            } catch (IOException e) {
+                Log.d("TAG", "File Exception " + e.getMessage());
+            }
+        });
+        loadFile.setOnClickListener(v -> loadSavedVideo());
+
+        saveVideoToCache();
     }
 
+    // TODO : Save video to temporary directory
     private void saveVideoToCache() {
         try {
+            String fileName = "Dummy" + System.currentTimeMillis();
             InputStream inputStream = getResources().openRawResource(R.raw.fi);
-            File tempDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "sample.mp4");
-            OutputStream outputStream = new FileOutputStream(tempDir);
+            File tempFile = File.createTempFile(fileName, ".mp4", getCacheDir());
+            OutputStream outputStream = new FileOutputStream(tempFile);
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
             }
-            fileName = tempDir;
+            tempoFile = tempFile;
             outputStream.close();
             inputStream.close();
         } catch (IOException e) {
-            Log.d("TAG","File name " + e.getMessage());
+            Log.d("TAG","File Exception " + e.getMessage());
         }
     }
     private void loadSavedVideo(){
-        // Display the video from the temporary file in the VideoView
-        if(fileName != null){
-            videoView.setVideoURI(Uri.fromFile(fileName));
+        if(finalFile != null){
+            videoView.setVideoURI(Uri.fromFile(finalFile));
             videoView.setMediaController(new MediaController(this));
             videoView.requestFocus();
             videoView.start();
@@ -76,4 +98,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
 }
+
+
